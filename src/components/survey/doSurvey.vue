39e14603dbca571b7b5cc5d7ebd0f6a95a283def
Nguyen Thanh Tuan <anhmaygalam15.tn@gmail.com>
2022-03-01 14:07:06 +0700

    optimize code

--- src/components/survey/doSurvey.vue
+++ src/components/survey/doSurvey.vue
@@ -12,7 +12,71 @@ import "survey-vue/modern.min.css";
 import {Survey,StylesManager, Model } from "survey-vue"
 
 StylesManager.applyTheme("modern");
-let surveyJson = window.localStorage.getItem("YourStorageName"); 
+let surveyJson = {
+ "pages": [
+  {
+   "name": "Trang 1",
+   "elements": [
+    {
+     "type": "text",
+     "name": "question1",
+     "title": "Bạn tên là gì?",
+     "isRequired": true
+    },
+    {
+     "type": "checkbox",
+     "name": "question2",
+     "title": "Ai là người đẹp trai nhất?",
+     "isRequired": true,
+     "choices": [
+      "item1",
+      "item2",
+      "item3"
+     ]
+    },
+    {
+     "type": "radiogroup",
+     "name": "question3",
+     "title": "Bạn có bao nhiêu người yêu!!!",
+     "isRequired": true,
+     "choices": [
+      "item1",
+      "item2",
+      "item3"
+     ]
+    }
+   ]
+  },
+  {
+   "name": "Trang 2",
+   "elements": [
+    {
+     "type": "text",
+     "name": "question4",
+     "title": "Bạn là ai hả?",
+     "isRequired": true
+    },
+    {
+     "type": "checkbox",
+     "name": "question5",
+     "title": "Bạn có những gì?",
+     "isRequired": true,
+     "choices": [
+      "item1",
+      "item2",
+      "item3"
+     ]
+    },
+    {
+     "type": "comment",
+     "name": "question6",
+     "title": "Tại sao bạn ghét tôi?",
+     "isRequired": true
+    }
+   ]
+  }
+ ]
+} 
 
 export default {
   name: 'doSurvey',
